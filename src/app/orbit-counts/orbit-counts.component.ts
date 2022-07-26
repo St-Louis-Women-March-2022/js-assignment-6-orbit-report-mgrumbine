f909b03af8163701a28f90fbb57f8602e6f17da7
mgrumbine <you@example.com>
2022-07-26 11:25:09 -0600

    Finished requirement 4 and updated the search feature (including the operational)

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -17,10 +17,18 @@ export class OrbitCountsComponent implements OnInit {
 
   countByType(type: string): number {
 	let count = 0;
+	console.log(type);
 	if (this.satellites) {
 	  for (let i = 0; i < this.satellites.length; i++) {
 		 if (this.satellites[i].type === type) {
+			console.log(this.satellites[i].type)
+			console.log(type)
+			console.log(this.satellites[i].type === type)
 			count++;
+		 } else {
+			console.log(this.satellites[i].type)
+			console.log(type)
+			console.log(this.satellites[i].type === type)
 		 }
 	  }
 	}
